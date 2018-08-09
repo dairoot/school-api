bd2dbd3d79ffb2f46b978b28896123b41ddbee2f
dairoot <623815825@qq.com>
2018-08-09 13:50:50 +0800

    添加python版本

--- setup.py
+++ setup.py
@@ -6,14 +6,17 @@ from setuptools import setup, find_packages
 
 setup(
     name="School-Api",
-    version="1.0",
     author="dairoot",
+    version="1.0",
+    license='MIT',
     author_email="623815825@qq.com",
     description="School SDK for Python",
     url='https://github.com/dairoot/school-api',
     packages=find_packages(),
     package_data={'school_api': ['check_code/theta.dat'], },
     include_package_data=True,
+    platforms='any',
+    zip_safe=False,
 
     install_requires=[
         'requests',
@@ -22,6 +25,19 @@ setup(
         'Image',
         'numpy'
     ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
 
-    zip_safe=False
 )
