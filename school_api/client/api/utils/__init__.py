1897345634b583aec4fa52b324be64cf372b279f
dairoot <623815825@qq.com>
2018-12-11 23:37:40 +0800

    修复获取__VIEWSTATE问题

--- school_api/client/api/utils/__init__.py
+++ school_api/client/api/utils/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import re
 from bs4 import BeautifulSoup
+from school_api.exceptions import SchoolException
 
 
 def get_alert_tip(html):
@@ -13,6 +14,10 @@ def get_alert_tip(html):
 def get_view_state_from_html(html):
     ''' 获取 __VIEWSTATE 值 '''
     pre_soup = BeautifulSoup(html, "html.parser")
-    view_state = pre_soup.find(
-        attrs={"name": "__VIEWSTATE"})['value']
+    view_state_soup = pre_soup.find(attrs={"name": "__VIEWSTATE"})
+    try:
+        view_state = view_state_soup['value']
+    except TypeError:
+        raise SchoolException('view_state','', '获取view_state失败')
+
     return view_state
